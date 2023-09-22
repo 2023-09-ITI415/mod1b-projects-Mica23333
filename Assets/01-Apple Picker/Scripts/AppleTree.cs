87a1c8a58a7072ff49a83ffc0ae9adbc00886a4f
Mica23333 <micayyq@gmail.com>
2023-09-21 23:56:03 -0400

    Create apple falling
    Create falling apples and physics layer and Stopping Apples If They Fall Too Far

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AppleTree : MonoBehaviour
+{
+    public static float bottomY = -20f;
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+[Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 10f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirection = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrop = 1f;
+
+    
+    void Start()
+    // Dropping apples every second
+    {
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrop);
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+   
+
+
+// Update is called once per frame
+void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        pos.x += 1.0f * 0.01f;
+        pos.x += 0.01f;
+        transform.position = pos;
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+    void FixedUpdate()
+    {
+        // Changing Direction Randomly
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; // Change direction
+        }
+     }
+}
